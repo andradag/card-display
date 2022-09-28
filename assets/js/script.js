ff9232a2bd4f863f1f187c1528bcad8be94ce71e
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-28 16:42:05 +0100

    checking if the click comes from the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,13 @@ const revealNumber = function (event) {
   // get target element
   const target = event.target;
 
-  // get data attributes of target
-  const number = target.dataset.number;
-  const state = target.dataset.state;
-  console.log(number, state);
-
   // check if click is coming from the box div
-  console.log("clicked");
+  if (target.matches(".box")) {
+    // get data attributes of target
+    const number = target.dataset.number;
+    const state = target.dataset.state;
+    console.log(number, state);
+  }
 };
 
 // add click event listener to parent container div
