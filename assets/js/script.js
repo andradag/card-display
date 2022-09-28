f6c2db44bef40fb82c1742fd0b9ca7b8de5271bd
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-28 16:37:53 +0100

    get data attributes of target

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,15 @@
 const containerDiv = document.querySelector(".container");
 
 // call back function of a click event
-const revealNumber = function () {
+const revealNumber = function (event) {
+  // get target element
+  const target = event.target;
+
+  // get data attributes of target
+  const number = target.dataset.number;
+  const state = target.dataset.state;
+  console.log(number, state);
+
   // check if click is coming from the box div
   console.log("clicked");
 };
